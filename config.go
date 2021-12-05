15458b7f429bfa7e58110ef47fdaaabc0e56be97
CG-SS <criguiss@uol.com.br>
2021-12-04 21:19:25 -0300

    Creation of the ClientConfig struct

--- config.go
+++ config.go
@@ -1,1 +1,14 @@
 package organisation_api
+
+import (
+	"log"
+	"net/url"
+)
+
+type ClientConfig struct {
+	RootUrl        *url.URL
+	InfoLog        *log.Logger
+	ErrorLog       *log.Logger
+	DebugLog       *log.Logger
+	IsDebugEnabled bool
+}
