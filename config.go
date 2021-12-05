fc2f254e2615dde22089cdde7bca10c6c80f4cd0
CG-SS <criguiss@uol.com.br>
2021-12-04 21:29:22 -0300

    Defining the default client configs

--- config.go
+++ config.go
@@ -3,6 +3,7 @@ package organisation_api
 import (
 	"log"
 	"net/url"
+	"os"
 )
 
 type ClientConfig struct {
@@ -21,3 +22,19 @@ var defaultRootUrl = func() *url.URL {
 
 	return d
 }()
+
+var DefaultConfig = &ClientConfig{
+	RootUrl:        defaultRootUrl,
+	InfoLog:        log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+	ErrorLog:       log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+	DebugLog:       nil,
+	IsDebugEnabled: false,
+}
+
+var DebugConfig = &ClientConfig{
+	RootUrl:        defaultRootUrl,
+	InfoLog:        log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+	ErrorLog:       log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+	DebugLog:       log.New(os.Stdout, "DEBUG\t", log.Ldate|log.Ltime),
+	IsDebugEnabled: true,
+}
