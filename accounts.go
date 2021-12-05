3085ec5c4799931b91908050acc60b221369e929
CG-SS <criguiss@uol.com.br>
2021-12-04 21:41:12 -0300

    Creation of the FetchAccount function

--- accounts.go
+++ accounts.go
@@ -3,6 +3,7 @@ package organisation_api
 import (
 	"bytes"
 	"encoding/json"
+	"path"
 )
 
 const accountsPath = "accounts"
@@ -24,6 +25,28 @@ func (c *OrganisationApiClient) CreateAccount(data AccountData) (*AccountData, e
 	}
 
 	resp, err := c.Post(requestUrl.String(), "application/json", bytes.NewBuffer(jsonValue))
+
+	if err != nil {
+		logMsg(c.ClientConfig.ErrorLog, err.Error())
+		return nil, err
+	}
+
+	defer closeBody(resp.Body)
+
+	return fetchAccountDataFromBody(resp)
+}
+
+func (c *OrganisationApiClient) FetchAccount(id string) (*AccountData, error) {
+	requestUrl, err := buildAccountsUrl(c)
+
+	if err != nil {
+		logMsg(c.ClientConfig.ErrorLog, err.Error())
+		return nil, err
+	}
+
+	requestUrl.Path = path.Join(requestUrl.Path, id)
+
+	resp, err := c.Get(requestUrl.String())
 
 	if err != nil {
 		logMsg(c.ClientConfig.ErrorLog, err.Error())
